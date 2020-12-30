07922ad2b67a07e122e0560421aff09c40152856
AndreaPineda-3010758 <74080247+AndreaPineda-3010758@users.noreply.github.com>
2020-12-30 18:49:05 -0500

    commit all

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,32 @@
+//Import the React and ReactDOM Libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
 
+{/*function getButtonText(){
+    return 'Click on me!';
+} */}
+//Create a react component
+const App = () => {
+    {/*const buttonText='Click Me!'; */}
+    const buttonText={text:'Click me'};
+    const labelText='Enter name:';
+    const style={backgroundColor: 'blue', color:'white'}
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      {/*<button style="background-color: blue; color:white;">Submit</button>*/}
+      {/*<button style={{backgroundColor: 'blue', color:'white'}}>{buttonText}</button> */}
+      {/*<button style={{backgroundColor: 'blue', color:'white'}}>{getButtonText()}</button> */}
+      <button style={style}>{buttonText.text}</button>
+    </div>
+  );
+};
+
+//Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
